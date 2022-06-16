2e78c99ddfd0eb907a32bb6114c105c0b5ed567f
jantunenv <ville.e.jantunen@gmail.com>
2022-06-16 21:19:42 +0300

    parallelized brute force sin solver, tested that it works, performance not tested yet

--- fitter_routines.f95
+++ fitter_routines.f95
@@ -1,4 +1,5 @@
 module fitter_routines
+use OMP_LIB
 contains
 	subroutine sin_fit_brute_force(x, y, n, a, w, p, c, atol, wtol, ptol, ctol, an, wn, pn, cn)
 		!Least square sum fit for a*sin(x*w + p) + c
@@ -10,7 +11,7 @@ contains
 		real(8), intent(inout) :: a, w, p, c
 		integer :: i, j, k, l
 		real(8) :: r(n), best_r, r_tot, a0, w0, p0, c0
-		real(8) :: atest, wtest, ptest, ctest
+		real(8) :: atest, wtest, ptest, ctest, final_best_r, besta, bestw, bestp, bestc
 
 		a0 = a - atol
 		w0 = w - wtol
@@ -18,7 +19,10 @@ contains
 		c0 = c - ctol
 
 		best_r = 9999999999.0
+		final_best_r = 999999999.9
 
+		!$OMP PARALLEL SHARED(final_best_r, a,w,p,c,) PRIVATE(i, j, k, l, besta, beastw, bestp, bestc, atest, wtest, ptest, ctest, best_r, r, r_tot)
+		!$OMP DO
 		do i=1,an
 			atest = a0 + i*(2*atol/an)
 			do j=1,wn
@@ -33,15 +37,26 @@ contains
 						r_tot = sum(r)
 						if(r_tot < best_r) then
 							best_r = r_tot
-							a = atest
-							w = wtest
-							p = ptest
-							c = ctest
+							besta = atest
+							bestw = wtest
+							bestp = ptest
+							bestc = ctest
 						end if 
 					end do
 				end do
 			end do
 		end do
+		!$OMP END DO
+		!$OMP CRITICAL
+		if(best_r < final_best_r) then
+			final_best_r = best_r
+			a = besta
+			w = bestw
+			p = bestp
+			c = bestc
+		end if
+		!$OMP END CRITICAL
+		!$OMP END PARALLEL
 
 	end subroutine sin_fit_brute_force
 end module fitter_routines
